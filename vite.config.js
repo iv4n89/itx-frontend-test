f7718b6a305e8a14159bd677f0a73e31dd8a7ba6
iv4n89 <ivan.betanzos89@gmail.com>
2026-06-17 19:45:26 +0200

    chore: js config and aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@/core": path.resolve(__dirname, "src/core"),
+      "@/ui": path.resolve(__dirname, "src/ui"),
+    },
+  },
+});
