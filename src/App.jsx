8b436f84e49b4af5de34f060c2fcd2c0374c93ed
iv4n89 <ivan.betanzos89@gmail.com>
2026-06-17 21:16:57 +0200

    feat: cart context

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import "./App.css";
 import { QueryProviderClient } from "./ui/common/util/query-client/query-client-provider";
 import { ReactQueryDevtools } from "node_modules/@tanstack/react-query-devtools/build/legacy/_tsup-dts-rollup";
 import { Routes } from "./ui/common/routes/routes";
+import { CartProvider } from "./ui/common/context/cart-context/cart-context-provider";
 
 function App() {
   return (
     <QueryProviderClient>
-      <RouterProvider router={Routes} />
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+      <CartProvider>
+        <RouterProvider router={Routes} />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+        />
+      </CartProvider>
     </QueryProviderClient>
   );
 }
