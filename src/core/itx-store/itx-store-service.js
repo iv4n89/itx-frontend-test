09fa3d2742dbc1f34fa485de3bb64c603416ec1f
iv4n89 <ivan.betanzos89@gmail.com>
2026-06-17 20:33:08 +0200

    feat: completed repository and service

--- src/core/itx-store/itx-store-service.js
+++ src/core/itx-store/itx-store-service.js
@@ -3,3 +3,11 @@ import { ItxStoreRepository } from "./itx-store-repository";
 export const getProductList = async () => {
   return await ItxStoreRepository.getProductList();
 };
+
+export const getProductDetails = async (productId) => {
+  return await ItxStoreRepository.getProductDetails(productId);
+};
+
+export const addToCart = async ({ id, colorCode, storageCode }) => {
+  return await ItxStoreRepository.addToCart({ id, colorCode, storageCode });
+};
