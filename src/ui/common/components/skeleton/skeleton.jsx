64c53d6c88f10cc02ffa73644c7ae680c0a70366
iv4n89 <ivan.betanzos89@gmail.com>
2026-06-17 21:28:34 +0200

    feat: skeleton

---
+++ src/ui/common/components/skeleton/skeleton.jsx
@@ -0,0 +1,14 @@
+import "./skeleton.css";
+
+export const Skeleton = ({ borderRadius = 8, width, height, className }) => {
+  return (
+    <div
+      className={`skeleton ${className || ""}}`}
+      style={{
+        borderRadius,
+        width,
+        height,
+      }}
+    ></div>
+  );
+};
